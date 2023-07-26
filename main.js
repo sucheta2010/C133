b25ad3f4480b629450ee259f584e0538628dce90
sucheta2010 <115158213+sucheta2010@users.noreply.github.com>
2023-07-26 18:14:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+x = 0;
+y = 0;
+
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("status").innerHTML = "System is listening please speak";
+    recognition.start();
+}
+
+
+function setup() {
+    canvas = createCanvas(900, 600);
+}
+
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+  }
+}
+
+
+
